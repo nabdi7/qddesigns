efd66abcc968ecab2c5590d59ca15f0c87785889
nabdi7 <36966910+nabdi7@users.noreply.github.com>
2024-06-07 21:43:03 -0700

    styling

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,9 +1,9 @@
-"use client";
+'use client';
 
-import { useState } from "react";
-import Image from "next/image";
-import Link from "next/link";
-import Button from "../button/Button";
+import { useState } from 'react';
+import Link from 'next/link' 
+import Image from 'next/image'
+import Button from '../button/Button'
 
 const Menu = () => {
   const [open, setOpen] = useState(false);
@@ -16,40 +16,40 @@ const Menu = () => {
   };
 
   return (
-    <div className="">
+    <div className=''>
       {open && (
         <Image
-          src="/close.svg"
-          alt="close"
+          src='/close.svg'
+          alt='close'
           width={28}
           height={28}
-          className="cursor-pointer"
+          className='cursor-pointer'
           onClick={handleCloseMenu}
         />
       )}
       {!open && (
         <Image
-          src="/menu.svg"
-          alt="menu"
+          src='/menu.svg'
+          alt='menu'
           width={28}
           height={28}
-          className="cursor-pointer"
+          className='cursor-pointer'
           onClick={handleToggleMenu}
         />
       )}
       {open && (
-        <div className="absolute p-5 bg-white shadow-lg left-0 top-20 w-full flex flex-col gap-8">
-          <Link href="/about" className="cursor-pointer transition-all hover:font-bold"> About </Link>
-          <Link href="/packages" className="cursor-pointer transition-all hover:font-bold"> Packages </Link>
-          <Link href="/services" className="cursor-pointer transition-all hover:font-bold"> Services </Link>
-          <Link href="/projects" className="cursor-pointer transition-all hover:font-bold"> Projects </Link>
-          <Link href="/contact" className="cursor-pointer transition-all hover:font-bold"> Contact </Link>
+        <div className='absolute p-5 bg-white shadow-lg left-0 top-20 w-full flex flex-col gap-8'>
+          <Link href='/about' className='cursor-pointer transition-all hover:font-bold'> About </Link>
+          <Link href='/packages' className='cursor-pointer transition-all hover:font-bold'> Packages </Link>
+          <Link href='/services' className='cursor-pointer transition-all hover:font-bold'> Services </Link>
+          <Link href='/projects' className='cursor-pointer transition-all hover:font-bold'> Projects </Link>
+          <Link href='/contact' className='cursor-pointer transition-all hover:font-bold'> Contact </Link>
           <Button 
-            type="button" 
-            title="Get A Quote" 
-            variant="button-color" 
-            shape="rectangle" 
-            href="/getquote" 
+            type='button'
+            title='Get A Quote' 
+            variant='button-color'
+            shape='rectangle' 
+            href='/getquote' 
           />
         </div>
       )}
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -6,31 +6,31 @@ import Button from '../button/Button'
 
 const Navbar = () => {
   return (
-    <nav className="flexBetween max-container padding-container relative z-30 py-5">
-      <div className="h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
-        <div className="h-full flex items-center justify-between">
-          <Link href="/">
-            <Image src="/logo.png" alt="logo" width={150} height={150} />
+    <nav className='flexBetween max-container padding-container relative z-30 py-5'>
+      <div className='h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative'>
+        <div className='h-full flex items-center justify-between'>
+          <Link href='/'>
+            <Image src='/logo.png' alt='logo' width={150} height={150} />
           </Link>
-          <div className="lg:hidden">
+          <div className='lg:hidden'>
             <Menu />
           </div>
-          <div className="hidden lg:flex items-center gap-8 ">
+          <div className='hidden lg:flex items-center gap-8 '>
             
-            <Link href="/about" className="flex-center cursor-pointer transition-all hover:font-bold"> About </Link>
-            <Link href="/packages" className="flex-center cursor-pointer transition-all hover:font-bold"> Packages </Link>
-            <Link href="/services" className="flex-center cursor-pointer transition-all hover:font-bold"> Services </Link>
-            <Link href="/projects" className="flex-center cursor-pointer transition-all hover:font-bold"> Projects </Link>
-            <Link href="/contact" className="flex-center cursor-pointer transition-all hover:font-bold"> Contact </Link>
+            <Link href='/about' className='flex-center cursor-pointer transition-all hover:font-bold'> About </Link>
+            <Link href='/packages' className='flex-center cursor-pointer transition-all hover:font-bold'> Packages </Link>
+            <Link href='/services' className='flex-center cursor-pointer transition-all hover:font-bold'> Services </Link>
+            <Link href='/projects' className='flex-center cursor-pointer transition-all hover:font-bold'> Projects </Link>
+            <Link href='/contact' className='flex-center cursor-pointer transition-all hover:font-bold'> Contact </Link>
           </div>
           {/* button */}
-          <div className="hidden lg:flex items-center gap-8 ">
+          <div className='hidden lg:flex items-center gap-8 '>
             <Button 
-              type="button" 
-              title="Get A Quote" 
-              variant="button-color" 
-              shape="rectangle" 
-              href="/getquote" 
+              type='button' 
+              title='Get A Quote'
+              variant='button-color' 
+              shape='rectangle' 
+              href='/getquote'
             />
           </div>
         </div>
