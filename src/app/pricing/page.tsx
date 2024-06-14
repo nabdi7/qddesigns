19d51b1f2b71d2aba4d8d263b39962a0cd5bb77e
nabdi7 <36966910+nabdi7@users.noreply.github.com>
2024-06-14 03:13:14 -0700

    added faqs

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Pricing from '@/components/pricing/Pricing'
+import Faqs from '@/components/faq/Faqs'
 import Cta from '@/components/cta/Cta'
 
 const page = () => {
   return (
     <div>
       <Pricing />
+      <Faqs />
       <Cta />
     </div>
   )
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Projects from '@/components/projects/Projects'
+import Cta from '@/components/cta/Cta'
 
 const page = () => {
   return (
     <div>
       <Projects />
+      <Cta />
     </div>
   )
 }
