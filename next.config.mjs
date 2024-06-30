2356b0aeadc36ad50c77a7c0ce86f320e72d5a59
nabdi7 <36966910+nabdi7@users.noreply.github.com>
2024-06-30 12:00:32 -0700

    final changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: {
-    domains: ['localhost'],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        port: "",
-        pathname: '/dlehxkdtn/**',
-        // https://res.cloudinary.com/dlehxkdtn/image/upload/untitled2_kaggtq.png
-      },
-    ],
-  },
+  images: {unoptimized: true},
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: "res.cloudinary.com",
+  //       port: "",
+  //     },
+  //   ],
+  // },
 };
 
 export default nextConfig;
