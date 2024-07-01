6792e859a2eede1db50befdf1a2812514a4a7df4
nabdi7 <36966910+nabdi7@users.noreply.github.com>
2024-06-30 22:17:40 -0700

    final changes

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -40,7 +40,6 @@ const Projects: React.FC = () => {
         whileInView="visible"
         transition={{ duration: 1, delay: 0.5 }}
         viewport={{ once: true }}
-        className="animate_top rounded-lg bg-white p-4 pb-9 shadow-solid-8 dark:bg-blacksection"
       >
         <div className="mt-8 flex justify-center space-x-4">
           {["All", "Websites", "Apps"].map((category) => (
