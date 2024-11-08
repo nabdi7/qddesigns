2f903497ad3cb4edac7aa7704a6d8c112d0219f8
nabdi7 <36966910+nabdi7@users.noreply.github.com>
2024-11-08 14:59:51 -0800

    project changes

--- src/components/projects/AllProjects.tsx
+++ src/components/projects/AllProjects.tsx
@@ -25,7 +25,7 @@ export const projects: Project[] = [
   },
   {
     title: "Najah Family Childcare",
-    description: "A website for Bright Smiles Childcare, emphasizing safety and care for young children.",
+    description: "A website for Najah Family Childcare, located in Seattle WA emphasizing safety and care for young children.",
     image: "/najah.png",
     category: "Websites",
     url: "https://najah.vercel.app/",
@@ -36,7 +36,7 @@ export const projects: Project[] = [
     description: "A website for a mosque, serving the Muslim community in Kent, WA.",
     image: "https://res.cloudinary.com/dlehxkdtn/image/upload/alhudaweb_jeq48b.png",
     category: "Websites",
-    url: "https://najah.vercel.app/",
+    url: "https://www.alhudaislamiccenter.com/",
   },
   {
     title: "Hadiya Childcare",
@@ -45,13 +45,6 @@ export const projects: Project[] = [
     category: "Websites",
     url: "https://www.hadiyachildcare.com",
   },
-  // {
-  //   title: "Lovingcare",
-  //   description: "Arcu a neque congue finibus donec malesuada et purus melan bris",
-  //   image: "/lovingcare-logo-transparent.png",
-  //   category: "Websites",
-  //   url: "https://www.lovingcarechildcare.com",
-  // },
   {
     title: "Halal Zone",
     description: "An app that helps users find authentic halal food with ease based on their location.",
