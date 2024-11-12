1442d5194bd64664fcffa251e2afa78329979a23
nabdi7 <36966910+nabdi7@users.noreply.github.com>
2024-11-11 17:49:49 -0800

    project changes

--- src/components/projects/AllProjects.tsx
+++ src/components/projects/AllProjects.tsx
@@ -1,61 +1,73 @@
-
 import { Project } from "../types/projects";
 
 export const projects: Project[] = [
   {
     title: "Almaas Childcare",
-    description: "A website for Almaas Childcare, offering nurturing daycare services located in Kent, WA.",
-    image: "https://res.cloudinary.com/dlehxkdtn/image/upload/almaasweb_rnavrp.png",
+    description:
+      "A website for Almaas Childcare, offering nurturing daycare services located in Kent, WA.",
+    image:
+      "https://res.cloudinary.com/dlehxkdtn/image/upload/almaasweb_rnavrp.png",
     category: "Websites",
     url: "https://www.almaaschildcare.com",
   },
   {
     title: "Emotions",
     description: "An app facilitating emotional well-being management.",
-    image: "https://res.cloudinary.com/dlehxkdtn/image/upload/emotions_hrn0pz.png",
+    image:
+      "https://res.cloudinary.com/dlehxkdtn/image/upload/emotions_hrn0pz.png",
     category: "Apps",
     url: "https://emotions-web.vercel.app/",
   },
   {
     title: "Bright Smiles Childcare",
-    description: "A website for Bright Smiles Childcare, emphasizing safety and care for young children.",
-    image: "https://res.cloudinary.com/dlehxkdtn/image/upload/brightsmilesweb_qmycmr.png",
+    description:
+      "A website for Bright Smiles Childcare, emphasizing safety and care for young children.",
+    image:
+      "https://res.cloudinary.com/dlehxkdtn/image/upload/brightsmilesweb_qmycmr.png",
     category: "Websites",
     url: "https://www.brightsmileshomechildcare.com",
   },
   {
+    title: "Hiba Family Childcare",
+    description:
+      "A website for Hiba Family Childcare, Licensed family childcare in Seattle WA.",
+    image: "/hiba.png",
+    category: "Websites",
+    url: "https://hibafamilychildcare.com/",
+  },
+  {
     title: "Tigertastic Family Daycare",
     description: "A website for a daycare business located in Seattle, WA.",
-    image: "https://res.cloudinary.com/dlehxkdtn/image/upload/tigertasticweb_b7nz5y.png",
+    image:
+      "https://res.cloudinary.com/dlehxkdtn/image/upload/tigertasticweb_b7nz5y.png",
     category: "Websites",
     url: "https://tigertastic.vercel.app/",
   },
-  // {
-  //   title: "Najah Family Childcare",
-  //   description: "A website for Najah Family Childcare, located in Seattle WA emphasizing safety and care for young children.",
-  //   image: "/najah.png",
-  //   category: "Websites",
-  //   url: "https://najah.vercel.app/",
-  // },
 
   {
     title: "Alhuda Islamic Center",
-    description: "A website for a mosque, serving the Muslim community in Kent, WA.",
-    image: "https://res.cloudinary.com/dlehxkdtn/image/upload/alhudaweb_jeq48b.png",
+    description:
+      "A website for a mosque, serving the Muslim community in Kent, WA.",
+    image:
+      "https://res.cloudinary.com/dlehxkdtn/image/upload/alhudaweb_jeq48b.png",
     category: "Websites",
     url: "https://www.alhudaislamiccenter.com/",
   },
   {
     title: "Hadiya Childcare",
-    description: "A website for Hadiya Childcare, supporting holistic child development.",
-    image: "https://res.cloudinary.com/dlehxkdtn/image/upload/hadiyaweb_slrw1k.png",
+    description:
+      "A website for Hadiya Childcare, supporting holistic child development.",
+    image:
+      "https://res.cloudinary.com/dlehxkdtn/image/upload/hadiyaweb_slrw1k.png",
     category: "Websites",
     url: "https://www.hadiyachildcare.com",
   },
   {
     title: "Halal Zone",
-    description: "An app that helps users find authentic halal food with ease based on their location.",
-    image: "https://res.cloudinary.com/dlehxkdtn/image/upload/restaurant_v8mcbd.png",
+    description:
+      "An app that helps users find authentic halal food with ease based on their location.",
+    image:
+      "https://res.cloudinary.com/dlehxkdtn/image/upload/restaurant_v8mcbd.png",
     category: "Apps",
     url: "https://halalrestaurant.com",
   },
